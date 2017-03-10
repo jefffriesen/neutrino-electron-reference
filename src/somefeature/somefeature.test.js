ec18662d7cd169390cf64c14ea1bcc782addb7fe
Jeff Friesen <momoblackblack@gmail.com>
2017-03-10 11:05:38 -0800

    Adding component rendering sanity check

--- src/somefeature/somefeature.test.js
+++ src/somefeature/somefeature.test.js
@@ -1,5 +1,12 @@
-describe('somefeature', () => {
-  it('should work without having to be only in the test folder', () => {
-    expect(!false).toBe(true);
+import React from 'react';
+import { shallow } from 'enzyme';
+import Feature from './index';
+
+// Basic sanity check that the component renders. This could be included with
+// every component
+describe('<Feature />', () => {
+  it('should render correctly', () => {
+    const dom = shallow(<Feature />);
+    expect(dom).toHaveLength(1);
   });
 });
